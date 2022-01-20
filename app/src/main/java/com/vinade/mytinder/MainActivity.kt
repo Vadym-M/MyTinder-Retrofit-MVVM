9cab0da91d2064962c6d5041feb699713e8a864f
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-20 16:07:26 +0100

    init viewmodel & repository

--- app/src/main/java/com/vinade/mytinder/MainActivity.kt
+++ app/src/main/java/com/vinade/mytinder/MainActivity.kt
@@ -2,10 +2,26 @@ package com.vinade.mytinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.vinade.mytinder.api.RetrofitClient
+import com.vinade.mytinder.repository.PersonRepository
+import com.vinade.mytinder.viewmodel.PersonViewModel
+import com.vinade.mytinder.viewmodel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: PersonViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = PersonRepository(RetrofitClient.api)
+        viewModel = ViewModelProvider(this, ViewModelFactory(repository)).get(PersonViewModel::class.java)
+        viewModel.getRandomPerson()
+        viewModel.person.observe(this, {
+            Log.d("debug", "Person name: " + it.results[0].name)
+    })
     }
 }
