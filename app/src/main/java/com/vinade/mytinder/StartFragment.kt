da32dc43355b0aae9db38fd0883b20e0e8d9125e
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-20 22:37:51 +0100

    add fragments

---
+++ app/src/main/java/com/vinade/mytinder/StartFragment.kt
@@ -0,0 +1,30 @@
+package com.vinade.mytinder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StartFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            StartFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
