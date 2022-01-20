91b4521e66e2c05b39562e0eaf2578b7c7de6d64
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-20 16:08:19 +0100

    rename base url

--- app/src/main/java/com/vinade/mytinder/api/RetrofitClient.kt
+++ app/src/main/java/com/vinade/mytinder/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    const val BASE_URL = "https://randomuser.me/api/"
+    const val BASE_URL = "https://randomuser.me"
 
     val retrofitClient: Retrofit.Builder by lazy {
         Retrofit.Builder().baseUrl(BASE_URL)
