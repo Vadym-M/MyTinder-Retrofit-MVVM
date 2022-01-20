28232c440d457a9fc7ebe6c8cd4c274cb6c2353b
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-20 22:37:30 +0100

    add binding

--- app/src/main/java/com/vinade/mytinder/MainActivity.kt
+++ app/src/main/java/com/vinade/mytinder/MainActivity.kt
@@ -3,8 +3,10 @@ package com.vinade.mytinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
 import com.vinade.mytinder.api.RetrofitClient
+import com.vinade.mytinder.databinding.ActivityMainBinding
 import com.vinade.mytinder.repository.PersonRepository
 import com.vinade.mytinder.viewmodel.PersonViewModel
 import com.vinade.mytinder.viewmodel.ViewModelFactory
@@ -12,10 +14,18 @@ import com.vinade.mytinder.viewmodel.ViewModelFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: PersonViewModel
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.commit {
+                add(R.id.fragment_container, StartFragment())
+            }
+        }
 
         val repository = PersonRepository(RetrofitClient.api)
         viewModel = ViewModelProvider(this, ViewModelFactory(repository)).get(PersonViewModel::class.java)
@@ -23,5 +33,6 @@ class MainActivity : AppCompatActivity() {
         viewModel.person.observe(this, {
             Log.d("debug", "Person name: " + it.results[0].name)
     })
+
     }
 }
