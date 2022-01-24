d51640420504a14619e3a82be639ae8d76d94e23
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-24 20:13:15 +0100

    add Retrofit builder for another url

--- app/src/main/java/com/vinade/mytinder/api/RetrofitClient.kt
+++ app/src/main/java/com/vinade/mytinder/api/RetrofitClient.kt
@@ -2,15 +2,27 @@ package com.vinade.mytinder.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object RetrofitClient {
-    const val BASE_URL = "https://randomuser.me"
+    private const val BASE_URL_PERSON = "https://randomuser.me"
+    private const val BASE_URL_SENTENCE = "https://fakerapi.it"
 
-    val retrofitClient: Retrofit.Builder by lazy {
-        Retrofit.Builder().baseUrl(BASE_URL)
+    private val retrofitPerson: Retrofit.Builder by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL_PERSON)
             .addConverterFactory(GsonConverterFactory.create())
     }
+
+    private val retrofitSentence: Retrofit.Builder by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL_SENTENCE)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+    val apiSentence: PersonApi by lazy {
+        retrofitSentence.build().create(PersonApi::class.java)
+    }
+
     val api: PersonApi by lazy {
-        retrofitClient.build().create(PersonApi::class.java)
+        retrofitPerson.build().create(PersonApi::class.java)
+
     }
 }
