bcb667b1863260bfbe43232cf86150bb5610a8b7
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-01-24 20:13:35 +0100

    update PersonViewModel.kt

--- app/src/main/java/com/vinade/mytinder/viewmodel/PersonViewModel.kt
+++ app/src/main/java/com/vinade/mytinder/viewmodel/PersonViewModel.kt
@@ -1,24 +1,62 @@
 package com.vinade.mytinder.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.vinade.mytinder.model.Person
+import com.vinade.mytinder.model.PersonObject
 import com.vinade.mytinder.repository.PersonRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class PersonViewModel(private val repository: PersonRepository): ViewModel() {
 
-    private val _person = MutableLiveData<Person>()
-            val person:LiveData<Person>
-            get()= _person
+    init {
+        getRandomPerson()
+    }
+    val currentGender = MutableLiveData<String>()
 
-    fun getRandomPerson(){
+
+    private val _currentPerson = MutableLiveData<PersonObject>()
+            val currentPerson:LiveData<PersonObject>
+            get()= _currentPerson
+
+    private val _exception = MutableLiveData<String>()
+    val exception:LiveData<String>
+        get()= _exception
+
+    lateinit var person: PersonObject
+
+    private fun getRandomPerson(){
         CoroutineScope(Dispatchers.IO).launch {
-            val person = repository.getPerson()
-            _person.postValue(person.body())
+            try {
+                val currentPerson = getPersonObject()
+                if(::person.isInitialized){
+                    person = currentPerson!!
+                }else{
+                    _currentPerson.postValue(currentPerson!!)
+                    person = getPersonObject()!!
+                }
+            }catch (e: Exception){
+                _exception.postValue("Oops! An Error Occurred, Too Many Requests")
+            }
+
         }
+    }
+
+    private suspend fun getPersonObject(): PersonObject? {
+        val prsn = repository.getPerson()
+        val sentence = repository.getSentence()
+        if(prsn.isSuccessful && sentence.isSuccessful){
+          return PersonObject(prsn.body()!!.results[0], sentence.body()!!.data[0].content)
+        }
+        return null
+    }
+    fun getCurrentPerson(){
+        _currentPerson.value = person
+        getRandomPerson()
     }
 }
